26bc6ba391e1022bfb5bc6a5e972b5612fcb6c0b
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-06 19:59:33 -0800

    time interval.

--- fourth-section/src/components/Recorder/Recorder.tsx
+++ fourth-section/src/components/Recorder/Recorder.tsx
@@ -1,22 +1,49 @@
-import "./Recorder.css";
-import { useDispatch, useSelector } from "react-redux";
-import { start, selectDateStart } from "redux/recorder";
+import { useRef, useState, useEffect } from "react";
 import cx from "classnames";
 
+import { useDispatch, useSelector } from "react-redux";
+import { start, selectDateStart } from "redux/recorder";
+
+import "./Recorder.css";
+
+const addZero = (num: number) => (num < 10 ? `0${num}` : `${num}`);
+
 const Recorder = () => {
+  const [, setCount] = useState<number>(0);
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
   const started = dateStart !== "";
+  let interval = useRef<number>(0);
   const handleClick = () => {
     dispatch(start());
+    interval.current = window.setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
   };
+
+  useEffect(() => {
+    return () => {
+      window.clearInterval(interval.current);
+    };
+  }, []);
+
+  let seconds = started
+    ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
+    : 0;
+
+  const hours = seconds ? Math.floor(seconds / 60 / 60) : 0;
+  seconds -= hours * 60 * 60;
+  const minutes = seconds ? Math.floor(seconds / 60) : 0;
+  seconds -= minutes * 60;
 
   return (
     <div className={cx("recorder", { "recorder-started": started })}>
       <button onClick={handleClick} className="recorder-record">
         <span></span>
       </button>
-      <div className="recorder-counter">00:00:00</div>
+      <div className="recorder-counter">
+        {addZero(hours)}:{addZero(minutes)}:{addZero(seconds)}
+      </div>
     </div>
   );
 };
