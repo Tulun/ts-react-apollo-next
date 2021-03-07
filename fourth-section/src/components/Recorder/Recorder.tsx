0d1d79e9a65e3a1be2e74cc7c1d0f03172512cc0
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-06 19:48:09 -0800

    date start.

--- fourth-section/src/components/Recorder/Recorder.tsx
+++ fourth-section/src/components/Recorder/Recorder.tsx
@@ -1,16 +1,18 @@
 import "./Recorder.css";
 import { useDispatch, useSelector } from "react-redux";
 import { start, selectDateStart } from "redux/recorder";
+import cx from "classnames";
 
 const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
+  const started = dateStart !== "";
   const handleClick = () => {
     dispatch(start());
   };
 
   return (
-    <div className="recorder">
+    <div className={cx("recorder", { "recorder-started": started })}>
       <button onClick={handleClick} className="recorder-record">
         <span></span>
       </button>
