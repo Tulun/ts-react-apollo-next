5fd6c05c7349f4fe32960275628147974338ce01
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-06 19:46:34 -0800

    selector

--- fourth-section/src/components/Recorder/Recorder.tsx
+++ fourth-section/src/components/Recorder/Recorder.tsx
@@ -1,9 +1,17 @@
 import "./Recorder.css";
+import { useDispatch, useSelector } from "react-redux";
+import { start, selectDateStart } from "redux/recorder";
 
 const Recorder = () => {
+  const dispatch = useDispatch();
+  const dateStart = useSelector(selectDateStart);
+  const handleClick = () => {
+    dispatch(start());
+  };
+
   return (
     <div className="recorder">
-      <button className="recorder-record">
+      <button onClick={handleClick} className="recorder-record">
         <span></span>
       </button>
       <div className="recorder-counter">00:00:00</div>
--- fourth-section/src/redux/store.ts
+++ fourth-section/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from "redux";
-import userEventsReducer from "./userEvents";
 import { composeWithDevTools } from "redux-devtools-extension";
+import userEventsReducer from "./userEvents";
+import recorderReducer from "./recorder";
 
 const rootReducer = combineReducers({
   userEvents: userEventsReducer,
+  recorder: recorderReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
