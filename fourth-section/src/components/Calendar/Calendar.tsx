bfa8aa623d75605374e32320bcf54b4b2f04d922
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-06 20:09:55 -0800

    show with two.

--- fourth-section/src/components/Calendar/Calendar.tsx
+++ fourth-section/src/components/Calendar/Calendar.tsx
@@ -5,12 +5,26 @@ const Calendar: React.FC = () => {
     <div className="calendar">
       <div className="calendar-day">
         <div className="calendar-day-label">
-          <span>1 February</span>
+          <span>2 February</span>
         </div>
         <div className="calendar-events">
           <div className="calendar-event">
             <div className="calendar-event-info">
               <div className="calendar-event-time">10:00 - 12:00</div>
+              <div className="calendar-event-title">Learning TypeScript</div>
+            </div>
+            <button className="calendar-event-delete-button">&times;</button>
+          </div>
+        </div>
+      </div>
+      <div className="calendar-day">
+        <div className="calendar-day-label">
+          <span>1 February</span>
+        </div>
+        <div className="calendar-events">
+          <div className="calendar-event">
+            <div className="calendar-event-info">
+              <div className="calendar-event-time">11:00 - 13:00</div>
               <div className="calendar-event-title">Learning TypeScript</div>
             </div>
             <button className="calendar-event-delete-button">&times;</button>
