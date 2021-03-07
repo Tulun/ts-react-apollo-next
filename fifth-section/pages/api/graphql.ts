42a650705918261b7920307885334e203953c7a8
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-07 13:37:33 -0800

    all return query etup.

--- fifth-section/pages/api/graphql.ts
+++ fifth-section/pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql, IResolvers } from "apollo-server-micro";
+import mysql from "serverless-mysql";
 
 const typeDefs = gql`
   enum TaskStatus {
@@ -34,10 +35,42 @@ const typeDefs = gql`
   }
 `;
 
-const resolvers: IResolvers = {
+interface ApolloContext {
+  db: mysql.ServerlessMysql;
+}
+
+enum TaskStatus {
+  active = "active",
+  completed = "completed",
+}
+
+interface Task {
+  id: number;
+  title: string;
+  status: TaskStatus;
+}
+
+interface TaskDBRow {
+  id: number;
+  title: string;
+  task_status: TaskStatus;
+}
+
+type TasksDbQueryResult = TaskDBRow[];
+const resolvers: IResolvers<any, ApolloContext> = {
   Query: {
-    tasks(parent, args, context) {
-      return [];
+    async tasks(parent, args, context): Promise<Task[]> {
+      const tasks = await context.db.query<TasksDbQueryResult>(
+        `SELECT id, title, task_status FROM tasks`
+      );
+
+      await db.end();
+
+      return tasks.map(({ id, title, task_status }) => ({
+        id,
+        title,
+        status: task_status,
+      }));
     },
     task(parent, args, context) {
       return null;
@@ -55,8 +88,16 @@ const resolvers: IResolvers = {
     },
   },
 };
+const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context: { db } });
 
 export const config = {
   api: {
