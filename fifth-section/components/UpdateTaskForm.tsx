6b12fec73cf961060c0da02eeee84a98ebbf4bda
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-08 16:08:53 -0800

    update task.

--- fifth-section/components/UpdateTaskForm.tsx
+++ fifth-section/components/UpdateTaskForm.tsx
@@ -1,4 +1,7 @@
+import { isApolloError } from "@apollo/client";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { useUpdateTaskMutation } from "../generated/graphql-frontend";
 
 interface Values {
   title: string;
@@ -6,13 +9,44 @@ interface Values {
 
 interface Props {
   initialValues: Values;
+  id: number;
 }
 
-const UpdateTaskForm: React.FC<Props> = ({ initialValues }) => {
+const UpdateTaskForm: React.FC<Props> = ({ initialValues, id }) => {
   const [values, setValues] = useState<Values>(initialValues);
 
+  const [updateTask, { loading, error }] = useUpdateTaskMutation();
+  const router = useRouter();
+  let errorMessage = "";
+  if (error) {
+    if (error.networkError) {
+      errorMessage = "A network error occurred, please try again.";
+    } else {
+      errorMessage = "Sorry, an error occurred.";
+    }
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+
+        try {
+          const result = await updateTask({
+            variables: { input: { title: values.title, id } },
+          });
+
+          if (result.data?.updateTask) {
+            router.push("/");
+          }
+        } catch (err) {
+          // log the error.
+          if (isApolloError(err)) {
+          }
+        }
+      }}
+    >
+      {error && <p className="alert-error">{errorMessage}</p>}
       <p>
         <label className="field-label">Title</label>
         <input
@@ -28,8 +62,8 @@ const UpdateTaskForm: React.FC<Props> = ({ initialValues }) => {
         />
       </p>
       <p>
-        <button className="button" type="submit">
-          Save
+        <button className="button" type="submit" disabled={loading}>
+          {loading ? "Loading" : "Save"}
         </button>
       </p>
     </form>
